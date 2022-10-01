2d3655185a78f8ac154e16035e98f7856ed6a4f1
Oleg <oleg.mukha.2002@gmail.com>
2022-10-01 15:01:45 +0300

    Created data-class for model. Created directories for database. Tested connection with database.

---
+++ src/data/Transaction.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.util.Date;
+
+public class Transaction {
+    Date currentDate;
+    String nameTransaction;
+    int senderCard;
+    int receiverCard;
+    double sum;
+    String description;
+}
---
+++ src/database/TransactionServiceMySQL.java
@@ -0,0 +1,7 @@
+package database;
+
+import database.dao.TransactionsDAO;
+
+public class TransactionServiceMySQL implements TransactionsDAO {
+
+}
